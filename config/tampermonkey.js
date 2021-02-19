4fbdda51aaee4f156fa181877a9105e9d9bee3a5
nutVI <1972404126@qq.com>
2021-02-19 23:25:24 +0800

    feat: 页面匹配和适应问题

--- config/tampermonkey.js
+++ config/tampermonkey.js
@@ -1,10 +1,12 @@
 // ==UserScript==
-// @name         targetName
+// @name         SZU mailbox
 // @namespace    http://tampermonkey.net/
 // @version      0.1
-// @description  targetDescription
-// @author       targetAuthor
-// @match        targetUrl
+// @description  在深圳大学校务信箱中添加了评论和回复的功能
+// @author       nut
+// @match        https://www1.szu.edu.cn/mailbox/view.asp*
+// @match        https://www1.szu.edu.cn/
+// @match        https://www1.szu.edu.cn/mailbox/
 // @grant        unsafeWindow
 // @require      https://cdn.jsdelivr.net/gh/nutVI/SZU_mailbox_forumFE/js_modules/vue-2.6.11/vue.min.js
 // @require      https://cdn.jsdelivr.net/gh/nutVI/SZU_mailbox_forumFE/js_modules/element-ui-2.15.0/index.min.js
--- src/main.js
+++ src/main.js
@@ -1,16 +1,19 @@
-import Vue from 'vue'
-loadStyle('https://cdn.jsdelivr.net/gh/nutVI/SZU_mailbox_forumFE/js_modules/element-ui-2.15.0/index.min.css')
+document.querySelector("body").style.paddingRight = "0"
 
-import App from './App.vue'
-import {
-  loadStyle
-} from './function'
+import api from './function/utils'
+api.loadStyle('https://cdn.jsdelivr.net/gh/nutVI/SZU_mailbox_forumFE/js_modules/element-ui-2.15.0/index.min.css')
 
 let father = document.getElementById('testVueToTamper') ||
   document.querySelector("body > table > tbody > tr:nth-child(2) > td > table > tbody > tr:nth-child(3) > td > table > tbody > tr:nth-child(2) > td")
+if (!api.getQueryVariable("id")) {
+  father = document.body
+}
 let divApp = document.createElement('div')
 divApp.id = 'app'
 father.append(divApp)
+
+import Vue from 'vue'
+import App from './App.vue'
 
 if (process.env.VUE_APP_ENVIRONMENT === "development") {
   const ElementUI = require('element-ui')
