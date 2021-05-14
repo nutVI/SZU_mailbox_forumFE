8dc81d61f54930660cc8e20df83352161c8ca40e
nutVI <1972404126@qq.com>
2021-05-15 02:33:43 +0800

    feat: 局部更新2

--- src/App.vue
+++ src/App.vue
@@ -4,9 +4,7 @@
       <SideBar v-show="navOut" />
     </transition>
     <Comment v-if="showURL == 1" />
-    <div
-      id="textLength"
-      style="
+    <div id="textLength" style="
         font-family: Microsoft Yahei;
         white-space: normal;
         position: fixed;
@@ -15,57 +13,55 @@
         opacity: 0;
         height: 0;
         font-size: 14px;
-      "
-    ></div>
+      "></div>
   </div>
 </template>
 
 <script>
-import Comment from "./views/Forum/Comment.vue";
-import SideBar from "./views/layout/SideBar.vue";
-import api from "./function/utils";
-import api2 from "./function/change";
+  import Comment from "./views/Forum/Comment.vue";
+  import SideBar from "./views/layout/SideBar.vue";
+  import api from "./function/utils";
+  import api2 from "./function/change";
 
-export default {
-  name: "App",
-  components: {
-    Comment,
-    SideBar,
-  },
-  data() {
-    return {
-      showURL: 0,
-      navOut: false,
-    };
-  },
-  beforeMount() {
-    setTimeout(() => {
-      this.navOut = true;
-    }, 0);
-    let linkElm = document.createElement("link");
-    linkElm.setAttribute("rel", "stylesheet");
-    linkElm.setAttribute("type", "text/css");
-    linkElm.setAttribute(
-      "href",
-      "https://at.alicdn.com/t/font_2377626_16z07jla638g.css"
-    );
-    document.head.appendChild(linkElm);
+  export default {
+    name: "App",
+    components: {
+      Comment,
+      SideBar,
+    },
+    data() {
+      return {
+        showURL: 0,
+        navOut: false,
+      };
+    },
+    beforeMount() {
+      setTimeout(() => {
+        this.navOut = true;
+      }, 0);
+      let linkElm = document.createElement("link");
+      linkElm.setAttribute("rel", "stylesheet");
+      linkElm.setAttribute("type", "text/css");
+      linkElm.setAttribute(
+        "href",
+        "https://at.alicdn.com/t/font_2377626_16z07jla638g.css"
+      );
+      document.head.appendChild(linkElm);
 
-    let a = document.querySelector(
-      "body > table > tbody > tr:nth-child(1) > td > table > tbody > tr:nth-child(3) > td:nth-child(1) > table > tbody > tr:nth-child(1) > td > a:nth-child(3)"
-    );
-    a &&
-      (a.onclick = () => {
-        api.httpJsonMethod("GET", "logout/", {});
-      });
+      let a = document.querySelector(
+        "body > table > tbody > tr:nth-child(1) > td > table > tbody > tr:nth-child(3) > td:nth-child(1) > table > tbody > tr:nth-child(1) > td > a:nth-child(3)"
+      );
+      a &&
+        (a.onclick = () => {
+          api.httpJsonMethod("GET", "logout/", {});
+        });
 
-    if (process.env.VUE_APP_ENVIRONMENT === "development") this.getMsg();
-    else
-      api
+      if (process.env.VUE_APP_ENVIRONMENT === "development") this.getMsg();
+      else
+        api
         .httpHtmlMethod(
           "GET",
-          "https://www1.szu.edu.cn/baoxiu/111.asp",
-          {},
+          "https://www1.szu.edu.cn/baoxiu/111.asp", {},
           "gb2312"
         )
         .then((res) => {
@@ -75,120 +71,127 @@ export default {
             this.$root.UUID = 0;
           }
         });
-    if (api.getQueryVariable("id")) {
-      this.showURL = 1;
-      api2.changePrePage(this.$root);
-    }
-  },
-  methods: {
-    getMsg() {
-      api
-        .httpJsonMethod("POST", "login/", {
-          ASP: api.getASPSESSION(),
-        })
-        .then((data) => {
-          this.$root.AVATAR = data.avatar;
-          this.$root.UUID = data.uuid;
-          this.$root.NICKNAME = data.nickname;
-          this.$root.MESSAGE = data.reply;
-          this.$root.LIKE = 0;
-        })
-        .catch((e) => {
-          this.$root.UUID = 0;
-          this.$message.error(e);
-        });
+      if (api.getQueryVariable("id")) {
+        this.showURL = 1;
+        api2.changePrePage(this.$root);
+        window.onpopstate = (e) => {
+          this.showURL = 0;
+          api2.popstateListen(e, this.$root)
+          this.$nextTick(function () {
+            this.showURL = 1;
+          })
+        }
+      }
     },
-  },
-};
+    methods: {
+      getMsg() {
+        api
+          .httpJsonMethod("POST", "login/", {
+            ASP: api.getASPSESSION(),
+          })
+          .then((data) => {
+            this.$root.AVATAR = data.avatar;
+            this.$root.UUID = data.uuid;
+            this.$root.NICKNAME = data.nickname;
+            this.$root.MESSAGE = data.reply;
+            this.$root.LIKE = 0;
+          })
+          .catch((e) => {
+            this.$root.UUID = 0;
+            this.$message.error(e);
+          });
+      },
+    },
+  };
 </script>
 
 <style>
-body {
-  overflow-y: scroll;
-}
+  body {
+    overflow-y: scroll;
+  }
 
-.el-tooltip__popper span {
-  color: #ffffff;
-  font-size: 12px;
-  line-height: 100%;
-}
+  .el-tooltip__popper span {
+    color: #ffffff;
+    font-size: 12px;
+    line-height: 100%;
+  }
 
-.el-message-box__btns span {
-  color: #000000;
-  font-size: 12px;
-  line-height: 100%;
-}
+  .el-message-box__btns span {
+    color: #000000;
+    font-size: 12px;
+    line-height: 100%;
+  }
 
-.buttonSelect span {
-  color: #fff;
-  font-family: 微软雅黑;
-  font-size: 14px;
-  line-height: 100%;
-}
+  .buttonSelect span {
+    color: #fff;
+    font-family: 微软雅黑;
+    font-size: 14px;
+    line-height: 100%;
+  }
 
-.text-buttonSelect span {
-  color: #409eff;
-  font-family: 微软雅黑;
-  font-size: 14px;
-  line-height: 100%;
-}
+  .text-buttonSelect span {
+    color: #409eff;
+    font-family: 微软雅黑;
+    font-size: 14px;
+    line-height: 100%;
+  }
 
-.text-buttonSelect:hover span {
-  color: #7cbbff;
-}
+  .text-buttonSelect:hover span {
+    color: #7cbbff;
+  }
 
-.text-buttonSelect:active span {
-  color: #006dda;
-}
+  .text-buttonSelect:active span {
+    color: #006dda;
+  }
 
-.text-dangerous-buttonSelect span {
-  color: #f56c6c;
-  font-family: 微软雅黑;
-  font-size: 14px;
-  line-height: 100%;
-}
+  .text-dangerous-buttonSelect span {
+    color: #f56c6c;
+    font-family: 微软雅黑;
+    font-size: 14px;
+    line-height: 100%;
+  }
 
-.text-dangerous-buttonSelect:active span {
-  color: #d33d3d;
-}
+  .text-dangerous-buttonSelect:active span {
+    color: #d33d3d;
+  }
 
-.text-buttonSelect-mini span {
-  color: #409eff;
-  font-family: 微软雅黑;
-  font-size: 12px;
-  line-height: 100%;
-}
+  .text-buttonSelect-mini span {
+    color: #409eff;
+    font-family: 微软雅黑;
+    font-size: 12px;
+    line-height: 100%;
+  }
 
-.text-buttonSelect-mini span:hover {
-  color: #7cbbff;
-}
+  .text-buttonSelect-mini span:hover {
+    color: #7cbbff;
+  }
 
-.text-buttonSelect-mini:active span {
-  color: #006dda;
-}
+  .text-buttonSelect-mini:active span {
+    color: #006dda;
+  }
 
-.text-link > a:hover {
-  text-decoration: none;
-}
+  .text-link>a:hover {
+    text-decoration: none;
+  }
 
-.text-link span {
-  color: #409eff;
-  font-family: 微软雅黑;
-  font-size: 14px;
-  line-height: 1.4;
-}
+  .text-link span {
+    color: #409eff;
+    font-family: 微软雅黑;
+    font-size: 14px;
+    line-height: 1.4;
+  }
 
-.text-link span:hover {
-  color: #7cbbff;
-}
+  .text-link span:hover {
+    color: #7cbbff;
+  }
 
-.navfade-enter-active,
-.navfade-leave-active {
-  transition: opacity 0.8s;
-}
+  .navfade-enter-active,
+  .navfade-leave-active {
+    transition: opacity 0.8s;
+  }
 
-.navfade-enter,
-.navfade-leave-to {
-  opacity: 0;
-}
+  .navfade-enter,
+  .navfade-leave-to {
+    opacity: 0;
+  }
 </style>
--- src/function/change.js
+++ src/function/change.js
@@ -2,6 +2,7 @@ import api from './utils'
 
 export default {
     changePrePage(root) {
+        root.POSTID = api.getPostID()
         let prePage, nextPage;
         prePage = document.getElementById('prePage') ||
             document.querySelector("body > table > tbody > tr:nth-child(2) > td > table > tbody > tr:nth-child(3) > td > table > tbody > tr:nth-child(1) > td > table > tbody > tr > td:nth-child(1) > a:nth-child(1)")
@@ -12,8 +13,6 @@ export default {
     },
 
     hotLoad(prePage, nextPage, root) {
-        root.POSTID = api.getPostID()
-
         this.changeInter(prePage, nextPage, root, prePage)
         this.changeInter(prePage, nextPage, root, nextPage)
     },
@@ -26,7 +25,6 @@ export default {
         currentPage.onclick = () => {
             console.log(url, search)
             api.httpHtmlMethod("GET", url, {}, "gb2312").then((res) => {
-                window.history.pushState(null, null, search);
 
                 let html = res.match(/width="90%" bgcolor="#FFFFFF">([\s\S]*?)<\/table>/)[1];
                 let id = res.matchAll(/\[<a href="(.*?)"/g)
@@ -39,9 +37,41 @@ export default {
 
                 prePage.href = (id.next()).value[1];
                 nextPage.href = (id.next()).value[1];
+                root.POSTID = api.getPostID()
 
+                window.history.pushState({
+                    table: html,
+                    prePage: prePage.href,
+                    nextPage: nextPage.href,
+                    postid: root.POSTID
+                }, document.title, search);
+
+                document.title = res.match(/(<title>=?)(.*?)(?=<\/title>)/)[2]
                 this.hotLoad(prePage, nextPage, root)
             })
         }
+    },
+
+    popstateListen(event, root) {
+        console.log(event.state, root)
+        root.POSTID = event.state.postid
+        
+        let prePage, nextPage;
+        prePage = document.getElementById('prePage') ||
+            document.querySelector("body > table > tbody > tr:nth-child(2) > td > table > tbody > tr:nth-child(3) > td > table > tbody > tr:nth-child(1) > td > table > tbody > tr > td:nth-child(1) > a:nth-child(1)")
+
+        nextPage = document.getElementById('nextPage') ||
+            document.querySelector("body > table > tbody > tr:nth-child(2) > td > table > tbody > tr:nth-child(3) > td > table > tbody > tr:nth-child(1) > td > table > tbody > tr > td:nth-child(1) > a:nth-child(2)")
+
+        prePage.href = event.state.prePage
+        nextPage.href = event.state.nextPage
+
+        let changeTable =
+            document.querySelector("body > table > tbody > tr:nth-child(2) > td > table > tbody > tr:nth-child(3) > td > table > tbody > tr:nth-child(2) > td > table")
+        if (changeTable) {
+            changeTable.innerHTML = event.state.table
+        }
+
+        this.hotLoad(prePage, nextPage, root)
     }
 }
